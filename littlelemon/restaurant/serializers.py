1afd17670fbeeaa4b0a7508a97d52532f1b0603d
aadridi <aadridi@gmail.com>
2024-12-19 16:30:55 +0100

    Installing DRF, writing serializers, define urls of both menu and booking APIs + views

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import menu, booking
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = menu
+        fields = ['title', 'price', 'inventory']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = booking
+        fields = '__all__'
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import menu, booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import viewsets, permissions
+
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index_start.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
